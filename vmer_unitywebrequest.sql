cb00c9919983e39227960f93183018f2f1e0f217
JoaquimCandieiro <joaquimpedroribeirocandieiro@gmail.com>
2026-05-06 12:25:35 +0100

    spripts additions commit

---
+++ vmer_unitywebrequest.sql
@@ -0,0 +1,31 @@
+drop database if exists vmer_unitywebrequest;
+
+create database if not exists vmer_unitywebrequest;
+
+use vmer_unitywebrequest;
+
+create table player_info(
+	id int primary key auto_increment,
+	name varchar(32) not null,
+	lives int,
+    health decimal(4, 1)
+);
+
+insert into player_info(name, lives, health)
+values(
+	'Player1',
+    3,
+    98.0
+),
+(
+	'Player2',
+    1,
+    70.5
+);
+
+select *
+from player_info;
+
+delete
+from player_info
+where id is null;
